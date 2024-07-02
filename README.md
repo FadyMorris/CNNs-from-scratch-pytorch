fc99f0507d36070cda498870d8260b1d84f933d7
Fady Morris Milad <fadymorris86@gmail.com>
2024-07-02 10:37:08 +0300

    feat:Major Architecture Fixes and Transfer Learning
    - Improve the architecture to match PyTorch
      original implementations.
    - Perform transfer learning by copying PyTorch
      pre-trained `IMAGENET1K_V1` weights from the
      original models.
    - Do the training using `ml.g4dn.xlarge` instance
      on Amazon SageMaker.

--- README.md
+++ README.md
@@ -5,22 +5,27 @@ This repository contains a code to build PyTorch famous CNN architectures.
 The notebooks were created for educational purposes, they show the architecures in detail and trace the inputs and the outputs of the networks.
 
 Architectures in this repository:
-* VGG
-* ResNet
+* VGG16
+* ResNet18
 
-The dataloader loads both CIFAR10 and CIFAR100 datasets and the code was run on hardware with the following specifications:
+The dataloader loads both CIFAR10 and CIFAR100 datasets and the code was run on Amazon SageMaker `ml.g4dn.xlarge` instance with the following hardware specifications:
 
 ```
-CPU model name	: 12th Gen Intel(R) Core(TM) i7-12650H
-CPU Count:  16
-GPU Model:  NVIDIA GeForce RTX 3070 Laptop GPU
+CPU model name	: Intel(R) Xeon(R) Platinum 8259CL CPU @ 2.50GHz
+CPU Count:  4
+GPU Model:  Tesla T4
 ```
 The models were built using Pytorch `Sequential` function and Python `OrderedDict`.
 
+## Transfer Learning
+These two models are identical to PyTorch impledmentation architecture. I copied the `IMAGENET1K_V1` pre-trained weights from PyTorch implementation and using the transfer learning technique, I trained the models to achieve higher performance scores.
+
+## Accelerated Computing
 The architecture parts are well-named for educational purposes.
 
 The code takes advantage of GPU computing and the dataloader is optimized to use the maximum number of processor cores and the GPU for transformations.
 
+## Resources
 The code was inspired by the following two blog posts:  
 * [Writing ResNet from Scratch in PyTorch](https://blog.paperspace.com/writing-resnet-from-scratch-in-pytorch/)  
 * [Writing VGG from Scratch in PyTorch](https://blog.paperspace.com/vgg-from-scratch-pytorch/)
--- src/custom_layers.py
+++ src/custom_layers.py
@@ -12,9 +12,9 @@ class ResidualBlock(nn.Module):
                         nn.Sequential(
                             OrderedDict(
                                 [
-                                    ("conv", nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)),
+                                    ("conv", nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)),
                                     ("batch_norm", nn.BatchNorm2d(out_channels)),
-                                    ("relu", nn.ReLU()),
+                                    ("relu", nn.ReLU(inplace=True)),
                                 ]
                             )
                         ),
@@ -24,7 +24,7 @@ class ResidualBlock(nn.Module):
                         nn.Sequential(
                             OrderedDict(
                                 [
-                                    ("conv", nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)),
+                                    ("conv", nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)),
                                     ("batch_norm", nn.BatchNorm2d(out_channels)),
                                 ]
                             )
@@ -38,14 +38,14 @@ class ResidualBlock(nn.Module):
             self.downsample = nn.Sequential(
                 OrderedDict(
                     [
-                        ("downsample_conv", nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)),
+                        ("downsample_conv", nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)),
                         ("downsample_batch_norm", nn.BatchNorm2d(out_channels)),
                     ]
                 )
             )
         else:
             self.downsample = None
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         # F(x)
@@ -61,28 +61,32 @@ class ResidualBlock(nn.Module):
         # Now we apply ReLU and return
         return self.relu(H)
 
-def get_conv_layer(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1, pool=False):
-        conv_block = nn.Sequential(
+def get_conv_layer(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1, bias=True):
+        conv_layer = nn.Sequential(
             OrderedDict(
                 [
-                ("conv", nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding)),
-                ("batch_norm", nn.BatchNorm2d(out_channels)),
-                ("relu", nn.ReLU()),
+                    ("conv", 
+                     nn.Conv2d(
+                         in_channels=in_channels, 
+                         out_channels=out_channels, 
+                         kernel_size=kernel_size, 
+                         stride=stride, 
+                         padding=padding, 
+                         bias=bias)
+                    ),
+                    ("batch_norm", nn.BatchNorm2d(out_channels)),
+                    ("relu", nn.ReLU()),
                 ]
             )
         )
-        conv_layer = OrderedDict([("conv_block", conv_block)])
-        
-        if pool:
-            conv_layer["max_pool"] = nn.MaxPool2d(kernel_size = 2, stride = 2)
-        return nn.Sequential(conv_layer)
+        return conv_layer 
 
 def get_fc_layer(in_channels, out_channels, dropout):
     sublayers = OrderedDict(
         [
-            ("dropout", nn.Dropout(dropout)),
             ("linear", nn.Linear(in_channels, out_channels)),
             ("relu", nn.ReLU()),
+            ("dropout", nn.Dropout(dropout)),
         ]
     )
     return nn.Sequential(sublayers)
